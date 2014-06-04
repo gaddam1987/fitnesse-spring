7af6aee129cc23c3acbc09ae4d6674e83ef18651
naresh <gaddam.1987@gmail.com>
2014-06-04 13:51:37 +0200

    first commit

---
+++ src/test/java/com/github/gaddam1987/AppTest.java
@@ -0,0 +1,13 @@
+package com.github.gaddam1987;
+
+import fitnesseMain.FitNesseMain;
+
+public class AppTest {
+
+    public static void main(String[] args) throws Exception {
+        FitNesseMain.main(new String[]{
+                "-p","8080"
+        });
+    }
+    
+}
---
+++ src/test/java/com/github/gaddam1987/fixture/TestFixture.java
@@ -0,0 +1,10 @@
+package com.github.gaddam1987.fixture;
+
+import fitlibrary.DoFixture;
+
+public class TestFixture extends DoFixture {
+
+    public boolean init() {
+        return false;
+    }
+}
